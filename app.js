1c5451f365547f80322c77e2b754f29773c0ed43
Sithumi Hirushika Kadigamuwage <sithumikadigamuwage@gmail.com>
2025-03-20 22:22:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();  
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello Express Js');  
+});
+
+app.get('/msg', (req, res) => {
+    res.send('Hello IT student');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on: ${port}`);
+});
